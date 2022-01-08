557beb96740d9448c501604d25d33f43a049b66e
Darpan Madlani <Darpan@proclink.com>
2022-01-08 09:14:10 +0530

    calculating daily wage of an employee in a month

---
+++ calculateDailyWageInMonth.sh
@@ -0,0 +1,44 @@
+PRESENT=1;
+PART_TIME=2;
+
+function getWorkingHour() {
+
+	case $1 in
+		$PRESENT)
+		wHour=8;
+		;;
+
+		$PART_TIME)
+		wHour=4;
+		;;
+
+		*)
+		wHour=0;
+		;;
+	esac;
+	echo $wHour;
+}
+
+MAX_WORKING_DAY=20;
+MAX_WORKING_HOUR=40;
+PAY_PER_HOUR=20;
+
+totalWorkingHour=0;
+day=0;
+while [[ $day -lt $MAX_WORKING_DAY && $(($totalWorkingHour+4)) -lt $MAX_WORKING_HOUR ]]
+do
+	workingHour=$( getWorkingHour $((RANDOM%3)) );
+	totalWorkingHour=$(($totalWorkingHour + $workingHour));
+	dailyWage[$day]=$(($workingHour * $PAY_PER_HOUR));
+	((day++));
+done
+
+salary=$(($totalWorkingHour * $PAY_PER_HOUR));
+
+echo "Employee monthly wage earned : $salary $ (Total working hour : $totalWorkingHour)"
+
+for index in ${!dailyWage[@]}
+do
+	echo "On Day $((index+1)) : " ${dailyWage[index]} "$";
+done
+
